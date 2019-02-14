0bbd17df2b13cb548265907ba2b6f23ed24ef2a8
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2019-02-14 16:10:32 +0900

    fix: create event checker

--- filewatch/runevent_windows.go
+++ filewatch/runevent_windows.go
@@ -32,25 +32,22 @@ func (e *Handler) RunEvent(ctx context.Context, exit chan<- struct{}, exitError 
 	defer close(c)
 
 	create := make(chan notify.EventInfo, 1)
-	defer close(c)
+	defer close(create)
 
 	// Set up a watchpoint listening on events
 	// Dispatch each create events separately to channel.
-	if e.UseFileEvent {
-		if err := notify.Watch(e.Directory, c, notify.FileNotifyChangeFileName, notify.FileNotifyChangeCreation); err != nil {
-			e.Logger.Error(err)
-			exitError <- err
-			return
-		}
-		defer notify.Stop(c)
-	} else {
-		if err = notify.Watch(e.Directory, create, notify.Create); err != nil {
-			e.Logger.Error(err)
-			exitError <- err
-			return
-		}
-		defer notify.Stop(create)
+	if err := notify.Watch(e.Directory, c, notify.FileNotifyChangeFileName, notify.FileNotifyChangeCreation); err != nil {
+		e.Logger.Error(err)
+		exitError <- err
+		return
 	}
+	defer notify.Stop(c)
+	if err = notify.Watch(e.Directory, create, notify.Create); err != nil {
+		e.Logger.Error(err)
+		exitError <- err
+		return
+	}
+	defer notify.Stop(create)
 
 	r := regexp.MustCompile(e.FilePattern)
 
@@ -63,71 +60,83 @@ func (e *Handler) RunEvent(ctx context.Context, exit chan<- struct{}, exitError 
 			return
 		case event := <-create:
 			e.Logger.Infof("create event detected: %s", event)
-			if !directory.IsExists(event.Path()) {
-				e.Logger.Info(event)
-				source := event.Path()
-				fileName := filepath.Base(source)
-				if !r.MatchString(fileName) {
-					break
-				}
-				fi, err := os.Stat(source)
-				if err != nil {
-					e.Logger.Errorf("error happen when checking %s. %s", source, err)
-					break
-				}
-				// replace symlink to generated file = latest
-				if fileName != e.SymlinkName {
-					e.Logger.Info(event)
-					e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
-					err = symlink.Replace(source, e.Dest)
-					if err != nil {
-						exitError <- err
-					}
-					current = fi
-				}
+			if directory.IsExists(event.Path()) {
+				e.Logger.Info("event was directory, skip and wait next ...")
+				break
 			}
-		case event := <-c:
 			source := event.Path()
 			fileName := filepath.Base(source)
 			if !r.MatchString(fileName) {
 				break
 			}
+			fi, err := os.Stat(source)
+			if err != nil {
+				e.Logger.Errorf("error happen when checking %s. %s", source, err)
+				break
+			}
+			if fileName == e.SymlinkName {
+				e.Logger.Infof("event filename was same as symlink, skip and wait next ...")
+				break
+			}
+			// replace symlink to generated file = latest
+			e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
+			err = symlink.Replace(source, e.Dest)
+			if err != nil {
+				e.Logger.Errorf("error happen when replacing symlink %s. %s", source, err)
+				break
+			}
+			current = fi
+		case event := <-c:
+			e.Logger.Infof("file event detected: %s", event)
+			if !e.UseFileEvent {
+				return
+			}
+			source := event.Path()
+			fileName := filepath.Base(source)
+			if !r.MatchString(fileName) {
+				e.Logger.Infof("event filename was not target, skip and wait next ...")
+				break
+			}
 			switch event.Event() {
-			case notify.FileActionAdded | notify.FileActionRenamedNewName:
-				e.Logger.Info(event)
+			case notify.FileActionAdded:
+				e.Logger.Info("file action added event, checking file exists ...")
 				fi, err := os.Stat(source)
 				if err != nil {
 					e.Logger.Errorf("error happen when checking %s. %s", source, err)
+					break
+				}
+				if fileName == e.SymlinkName {
+					e.Logger.Infof("event filename was same as symlink, skip and wait next ...")
 					break
 				}
 				// replace symlink to generated file = latest
-				if fileName != e.SymlinkName {
-					e.Logger.Info(event)
-					e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
-					err = symlink.Replace(source, e.Dest)
-					if err != nil {
-						exitError <- err
-					}
-					current = fi
+				e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
+				err = symlink.Replace(source, e.Dest)
+				if err != nil {
+					e.Logger.Errorf("error happen when replacing symlink %s. %s", source, err)
+					break
 				}
+				current = fi
 			case notify.FileActionRenamedNewName:
-				e.Logger.Info(event)
+				e.Logger.Info("file action added event, checking file exists ...")
 				fi, err := os.Stat(source)
 				if err != nil {
 					e.Logger.Errorf("error happen when checking %s. %s", source, err)
 					break
 				}
+				if fileName == e.SymlinkName {
+					e.Logger.Infof("event filename was same as symlink, skip and wait next ...")
+					break
+				}
+				// replace symlink to renamed file
 				if current == nil || fi.ModTime().After(current.ModTime()) {
-					// replace symlink to renamed file
-					if fileName != e.SymlinkName {
-						e.Logger.Info(event)
-						e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
-						err = symlink.Replace(source, e.Dest)
-						if err != nil {
-							exitError <- err
-						}
-						current = fi
+					e.Logger.Infof("Create/Replace symlink new: %s, old: %s ...", source, e.Dest)
+					err = symlink.Replace(source, e.Dest)
+					if err != nil {
+						e.Logger.Errorf("error happen when replacing symlink %s. %s", source, err)
+						break
 					}
+					current = fi
 				}
 			}
 		}
